cf7ae547edb915d6b55f9f1236f56292864af771
tkhs-0114 <tkhs.mail0114@gmail.com>
2025-08-06 16:30:02 +0900

    fix: 空の場合をundefinedに変更

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename: process.env.VITE_BASE_PATH?.replace(/\/$/, '') || '/',
+  basename: process.env.VITE_BASE_PATH?.replace(/\/$/, '') || undefined,
 } satisfies Config;
