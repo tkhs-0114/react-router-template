16720fbb9a310c839b0c0546a8ef6f8f2d64ea45
tkhs-0114 <tkhs.mail0114@gmail.com>
2025-08-06 10:13:25 +0900

    fix: ベースパスを環境変数に変更

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: process.env.NODE_ENV === "production" ? "/react-router1/" : "/",
+  base: process.env.VITE_BASE_PATH || "/",
 });
